5913dd36b3044dff6f4f7fac6853e1105b0493d0
Alex Kagai <alexkagai@outlook.com>
2020-12-09 18:25:14 +0300

    The following commit will: 1. Disable usage of the Vue-router 2. Add the usage of: 	- watch 	- computed 	- methods 	- mounted lifecycle hook 	- click events

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,64 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    @{{ user.username }} - {{ fullname }}
+    <strong> Followers</strong> {{ followers }}
+    <button @click="followUser"> follow </button>
+
   </div>
-  <router-view/>
 </template>
+
+
+<script>
+export default{
+  name: 'App',
+  data(){
+    return{
+      followers:0,
+      user:{
+        username:'alexkagai',
+        firstname: 'Alex',
+        lastname: 'Kagai',
+        email: 'alex@ak-tweet.com',
+        isAdmin: true
+      }
+    }
+  }, // end of data
+  watch:{
+    followers(currentCount, previousCount){
+      if(previousCount < currentCount){
+        console.log(`${this.user.username} has gained a new follower`)
+      }
+    }
+
+  }, // end of watch
+  computed:{
+    fullname(){
+      return `${this.user.firstname} ${this.user.lastname}`
+    }
+
+  }, //end of Computed
+  methods:{
+    followUser(){
+      this.followers++;
+    }
+  }, // end of Methods
+  mounted(){
+    this.followUser();
+    console.log(`${ this.user.username} just got followed`);
+  }
+}
+</script>
+
 
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
 }
 
 #nav {
